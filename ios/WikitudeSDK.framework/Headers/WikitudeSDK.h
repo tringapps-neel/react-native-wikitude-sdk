5f403eb5f683f618dab3cf691ae859d3e4e05ffd
Neel <tringapps@Tringappss-MacBook-Pro.local>
2020-11-07 13:02:09 +0530

    Replaced with new framework

--- ios/WikitudeSDK.framework/Headers/WikitudeSDK.h
+++ ios/WikitudeSDK.framework/Headers/WikitudeSDK.h
@@ -27,10 +27,7 @@ FOUNDATION_EXPORT const unsigned char WikitudeSDKVersionString[];
 //#import <WikitudeSDK/WTExternalCMMotionManagerDataAccessMode.h>
 
 #import <WikitudeSDK/ArchitectPlugin.hpp>
-<<<<<<< HEAD
-=======
 #import <WikitudeSDK/CameraFocusMode.hpp>
->>>>>>> 7a80d517418492d323a2b0529e1da11bec307318
 #import <WikitudeSDK/CameraFrame.hpp>
 #import <WikitudeSDK/CameraFrameInputPluginModule.hpp>
 #import <WikitudeSDK/CameraFramePlane.hpp>
@@ -44,10 +41,7 @@ FOUNDATION_EXPORT const unsigned char WikitudeSDKVersionString[];
 #import <WikitudeSDK/ErrorHandling.hpp>
 #import <WikitudeSDK/Geometry.hpp>
 #import <WikitudeSDK/ImageTarget.hpp>
-<<<<<<< HEAD
-=======
 #import <WikitudeSDK/ImageTargetType.hpp>
->>>>>>> 7a80d517418492d323a2b0529e1da11bec307318
 #import <WikitudeSDK/ImageTrackingPluginModule.hpp>
 #import <WikitudeSDK/InstantTarget.hpp>
 #import <WikitudeSDK/InstantTrackingPluginModule.hpp>
@@ -78,10 +72,7 @@ FOUNDATION_EXPORT const unsigned char WikitudeSDKVersionString[];
 #import <WikitudeSDK/RenderableCameraFrame.hpp>
 #import <WikitudeSDK/RenderableCameraFrameBucket.hpp>
 #import <WikitudeSDK/RenderingPluginModule.hpp>
-<<<<<<< HEAD
-=======
 #import <WikitudeSDK/SDKEdition.hpp>
->>>>>>> 7a80d517418492d323a2b0529e1da11bec307318
 #import <WikitudeSDK/SensorEvent.hpp>
 #import <WikitudeSDK/State.hpp>
 #import <WikitudeSDK/Timestamp.hpp>
